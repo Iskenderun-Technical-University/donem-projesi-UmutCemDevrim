e01a31bf5cd13d1b526b3c1abe414d2778462290
UmutCemDevrim <101496992+UmutCemDevrim@users.noreply.github.com>
2023-06-04 22:42:56 +0300

    Add files via upload

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text highestScore;
+    public Text currentScore;
+    public GameObject player;
+        
+    
+    void Start () {
+        if (player == null)
+            player = GameObject.FindWithTag("Player");
+		
+		int previousHighestScore = PlayerPrefs.GetInt("highestScore");
+		highestScore.text = "Highest Score: " + previousHighestScore;
+    }
+	
+	
+	void Update () {
+        currentScore.text = "Current Score: " + (int)player.transform.position.x;
+        if ((int) player.transform.position.x > int.Parse(highestScore.text.Substring(14))) {
+            highestScore.text = "Highest Score: " + (int)player.transform.position.x;
+        }         
+    }
+}
